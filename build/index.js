b0ff7f4da9c61105694285885f3d12a28df1a2ca
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-15 14:30:11 -0400

    initial files w/Sorter class

---
+++ build/index.js
@@ -0,0 +1,28 @@
+"use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    /* THE ABOVE CODE FUNCTION THE SAME
+    AS THE CODE BENEATH
+    collection : number[];
+     constructor(collection : number[]) {
+         this.collection = collection;
+     */
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length; j++) {
+                //Only works if collection is an array of numbers
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
